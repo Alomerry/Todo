3b77065f6e3882c58669be87e6603857b6f89944
Alomerry Wu <alomerry.wu@maiscrm.com>
2020-09-27 18:16:36 +0800

    use jwt

--- backend/src/main/java/app/WebApp.java
+++ backend/src/main/java/app/WebApp.java
@@ -1,13 +1,18 @@
 package app;
 
+import config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication()
-@ComponentScan({"controller","service"})
+@ComponentScan({"controller", "service"})
 @EnableMongoRepositories("dao")
+@EnableConfigurationProperties({
+        JwtConfig.class,
+})
 public class WebApp {
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
