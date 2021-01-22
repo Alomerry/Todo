7a3a713e09d4522c30a12abb675c413f23d218ae
Alomerry <alomerry@hotmail.com>
2021-01-23 01:18:14 +0800

    member: add ci

--- backend/src/main/java/dao/MemberDao.java
+++ backend/src/main/java/dao/MemberDao.java
@@ -5,6 +5,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MemberDao extends MongoRepository<Member, ObjectId> {
+
     Member findByNameAndPasswdAndIsDeleted(String name, String passwd, boolean isDeleted);
 
     Member findByNameAndIsDeleted(String name, boolean isDeleted);
